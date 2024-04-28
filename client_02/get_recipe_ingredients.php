a30a0cd35019a608a632513b34b75220a31430c9
Josef Razák <90528004+MasterkoCZ@users.noreply.github.com>
2024-04-28 22:52:24 +0200

    Update get_recipe_ingredients.php

--- client_02/get_recipe_ingredients.php
+++ client_02/get_recipe_ingredients.php
@@ -1,31 +1,22 @@
 <?php
-// get_recipe_ingredients.php
-
-// Připojení k API nebo načtení potřebných knihoven
 require_once("SmartCookClient.php");
 
 try {
-    // Získání ID receptu z parametru
     $recipe_id = isset($_GET['recipe_id']) ? $_GET['recipe_id'] : null;
 
-    // Kontrola, zda bylo poskytnuto ID receptu
     if ($recipe_id === null) {
         throw new Exception('Recipe ID is missing.');
     }
 
-    // Volání API pro získání ingrediencí konkrétního receptu
     $response = (new SmartCookClient)
-        ->sendRequest("recipes/{$recipe_id}/ingredients") // Změna URL na získání ingrediencí konkrétního receptu
+        ->sendRequest("recipes/{$recipe_id}/ingredients")
         ->getResponseData();
 
-    // Nastavení hlavičky Content-Type na JSON
     header('Content-Type: application/json');
 
-    // Výstup v JSON formátu
     echo json_encode($response);
 
 } catch (Exception $e) {
-    // V případě chyby vracíme chybovou zprávu
     echo json_encode(['error' => $e->getMessage()]);
 }
 ?>
